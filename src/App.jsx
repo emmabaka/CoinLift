d186b12fa25bbe80be4655ad7079367514cc152e
emmabaka <bakaemma88@gmail.com>
2023-06-19 19:38:55 +0300

    refactor routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home/Home";
 
 function App() {
-
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/CoinLift" element={<Layout />}>
           <Route index element={<Home />} />
         </Route>
       </Routes>
